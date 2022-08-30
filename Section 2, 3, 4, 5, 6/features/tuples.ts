ac87b549b5621e71974dddc639aa3dbe1ae54f2d
RaguRamanTB <aakash1956ragu@gmail.com>
2022-08-30 22:46:14 +0530

    Why Tuples?

--- Section 2, 3, 4, 5, 6/features/tuples.ts
+++ Section 2, 3, 4, 5, 6/features/tuples.ts
@@ -17,3 +17,14 @@ type Drink = [string, boolean, number];
 const pepsi: Drink = ["brown", true, 40];
 const sprite: Drink = ["clear", true, 40];
 const tea: Drink = ["brown", false, 0];
+
+const carSpecs: [number, number] = [400, 3354];
+// The above tuple doesn't mean anything about the number.
+// As engineers we cannot know which number corresponds to what key.
+// Hence, tuples are used only at specific usecases like processing CSV, etc.
+// At all other instances, JS Object is much more clear than tuples.
+
+const carStats = {
+  horsePower: 400,
+  weight: 3354,
+};
