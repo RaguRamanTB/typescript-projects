eda367a92ba283bf97a1db9b5e83dc6595ebe019
RaguRamanTB <aakash1956ragu@gmail.com>
2022-08-30 22:18:40 +0530

    Why typed arrays?

--- Section 2, 3, 4, 5/features/arrays.ts
+++ Section 2, 3, 4, 5/features/arrays.ts
@@ -4,3 +4,15 @@ const dates = [new Date(), new Date()];
 
 const carsByMake = [["f150"], ["corolla"], ["x1"], ["e250"]];
 const empty2DArray: string[][] = [];
+
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100);
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
