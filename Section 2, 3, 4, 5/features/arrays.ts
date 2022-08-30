f89f91e6bd9f0b5e6e3b4ee7447ce1b7d3086f1a
RaguRamanTB <aakash1956ragu@gmail.com>
2022-08-30 22:13:09 +0530

    Arrays in Typescript

---
+++ Section 2, 3, 4, 5/features/arrays.ts
@@ -0,0 +1,6 @@
+const carMakers = ["ford", "toyota", "bmw", "benz"];
+const emptyArray: string[] = [];
+const dates = [new Date(), new Date()];
+
+const carsByMake = [["f150"], ["corolla"], ["x1"], ["e250"]];
+const empty2DArray: string[][] = [];
