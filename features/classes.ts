6b80d845f34797d0d0a1875cca31f3935ae7b7db
RaguRamanTB <aakash1956ragu@gmail.com>
2022-09-01 18:50:22 +0530

    Instance method modifiers

--- features/classes.ts
+++ features/classes.ts
@@ -1,23 +1,29 @@
 class Vehicle {
-  drive(): void {
+  public drive(): void {
     console.log("Get Set Go!!!");
   }
 
-  honk(): void {
+  protected honk(): void {
     console.log("Beep Beep!!!");
   }
 }
 
 class Car extends Vehicle {
+  // You cannot change access modifiers when overwriting methods.
   drive(): void {
     console.log("Vroom... Get Set Go!!!");
   }
+
+  private shiftGear(): void {
+    console.log("Gear shifted!!!");
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.shiftGear();
+    this.honk();
+  }
 }
 
-const vehicle = new Vehicle();
-vehicle.drive();
-vehicle.honk();
-
 const car = new Car();
-car.drive();
-car.honk();
+car.startDrivingProcess();
