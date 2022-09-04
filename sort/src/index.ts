0437bac3a345e5910a42ed92968f8097516a714f
RaguRamanTB <aakash1956ragu@gmail.com>
2022-09-04 20:41:41 +0530

    Using Abstract Classes

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,10 +1,13 @@
-import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([0, 10, 5, -2]);
+numbersCollection.sort();
+
 const charactersCollection = new CharactersCollection("VasdxcAww");
+charactersCollection.sort();
+
 const linkedList = new LinkedList();
 linkedList.add(43);
 linkedList.add(1243);
@@ -12,15 +15,7 @@ linkedList.add(0);
 linkedList.add(3);
 linkedList.add(-5);
 linkedList.add(-43);
-
-let sorter = new Sorter(numbersCollection);
-sorter.sort();
-
-sorter = new Sorter(charactersCollection);
-sorter.sort();
-
-sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 
 console.log(numbersCollection.data);
 console.log(charactersCollection.data);
