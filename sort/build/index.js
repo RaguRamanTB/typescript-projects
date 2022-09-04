868cdba44867cdee926657bbe51a63f8ef5958f6
RaguRamanTB <aakash1956ragu@gmail.com>
2022-09-04 15:50:41 +0530

    Concurrent compilation and execution

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,5 +1,5 @@
 "use strict";
-console.log("Hello World!");
+console.log("Hello World! Ragu");
 const logSomething = () => {
     console.log("Hello World!");
 };
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,4 +1,4 @@
-console.log("Hello World!");
+console.log("Hello World! Ragu");
 
 const logSomething = () => {
   console.log("Hello World!");
