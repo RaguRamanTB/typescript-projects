2d3aad14111bae538204e694cddba83e6a83b12d
RaguRamanTB <aakash1956ragu@gmail.com>
2022-09-17 17:00:22 +0530

    Storing Event Listeners

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -6,6 +6,8 @@ interface UserProps {
 type Callback = () => {}; // Type alias
 
 export class User {
+  events: { [key: string]: Callback[] } = {};
+
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
