e5b71fb850f875e386c88973272b9c2563da9ade
RaguRamanTB <aakash1956ragu@gmail.com>
2022-09-17 16:49:16 +0530

    Optional Interface Properties

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,5 +2,8 @@ import { User } from "./models/User";
 
 const user = new User({ name: "Ragu", age: 22 });
 
+user.set({ name: "Aakash", age: 50 });
+user.set({ age: 25 });
+
 console.log(user.get("name"));
 console.log(user.get("age"));
