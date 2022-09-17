c0a926e1c6c71e5f2f4ee580f320d6c308fad6bd
RaguRamanTB <aakash1956ragu@gmail.com>
2022-09-17 20:37:26 +0530

    Dynamic Array Creation

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,8 +2,8 @@ import { User } from "./models/User";
 
 const user = new User({ name: "Ragu", age: 22 });
 
-user.set({ name: "Aakash", age: 50 });
-user.set({ age: 25 });
+user.on("change", () => {});
+user.on("change", () => {});
+user.on("sample", () => {});
 
-console.log(user.get("name"));
-console.log(user.get("age"));
+console.log(user);
