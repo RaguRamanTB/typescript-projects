6ed2fea0ec3d4fac7cf14ccd12aef5b084e49870
RaguRamanTB <aakash1956ragu@gmail.com>
2022-09-29 19:42:41 +0530

    Understanding REST Conventions

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,17 +1,3 @@
-import { User } from './models/User';
+import axios from 'axios';
 
-const user = new User({ name: 'Ragu', age: 22 });
-
-user.on('change', () => {
-  console.log('Change #1');
-});
-user.on('change', () => {
-  console.log('Change #2');
-});
-user.on('save', () => {
-  console.log('Save was triggered!');
-});
-
-user.trigger('change');
-user.trigger('save');
-user.trigger('ASDAS');
+axios.get('http://localhost:3000/users/1');
