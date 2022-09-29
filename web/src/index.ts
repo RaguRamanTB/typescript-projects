22f92466184350fbfcbcc36e6ec422ea81348383
RaguRamanTB <aakash1956ragu@gmail.com>
2022-09-29 19:51:33 +0530

    Successfully fetching model data

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,6 +1,8 @@
-import axios from 'axios';
+import { User } from './models/User';
 
-axios.post('http://localhost:3000/users', {
-  name: 'Ragu',
-  age: 22,
-});
+const user = new User({ id: 1 });
+user.fetch();
+
+setTimeout(() => {
+  console.log(user);
+}, 4000);
