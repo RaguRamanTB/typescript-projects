7e25ead1b95ea8d8a7c62cdd6f5cb989ea770a56
RaguRamanTB <aakash1956ragu@gmail.com>
2022-09-29 19:21:45 +0530

    Adding Triggers

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,9 +1,17 @@
-import { User } from "./models/User";
+import { User } from './models/User';
 
-const user = new User({ name: "Ragu", age: 22 });
+const user = new User({ name: 'Ragu', age: 22 });
 
-user.on("change", () => {});
-user.on("change", () => {});
-user.on("sample", () => {});
+user.on('change', () => {
+  console.log('Change #1');
+});
+user.on('change', () => {
+  console.log('Change #2');
+});
+user.on('save', () => {
+  console.log('Save was triggered!');
+});
 
-console.log(user);
+user.trigger('change');
+user.trigger('save');
+user.trigger('ASDAS');
