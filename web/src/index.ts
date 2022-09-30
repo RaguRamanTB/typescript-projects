99155b829005fdd516fe9907ea7a8da4992dc338
RaguRamanTB <aakash1956ragu@gmail.com>
2022-09-30 10:19:07 +0530

    A context issue

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,6 +2,10 @@ import { User } from './models/User';
 
 const user = new User({ name: 'New Record', age: 9999 });
 
+console.log(user.get('name'));
+
 user.on('change', () => {
-  console.log('Changed');
+  console.log('User was changed');
 });
+
+user.trigger('change');
