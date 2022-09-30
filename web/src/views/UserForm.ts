314ec0d2fd6c3cbffe8a3b07bfea59dbdd2e75c4
RaguRamanTB <aakash1956ragu@gmail.com>
2022-09-30 14:08:19 +0530

    Reading Input Text

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -14,8 +14,19 @@ export class UserForm {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick,
     };
   }
+
+  onSetNameClick = (): void => {
+    if (this.parent) {
+      const input = this.parent.querySelector('input');
+      if (input) {
+        const name = input.value;
+        this.model.set({ name });
+      }
+    }
+  };
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
@@ -28,7 +39,7 @@ export class UserForm {
         <div>User Name: ${this.model.get('name')}</div>
         <div>User Age: ${this.model.get('age')}</div>
         <input />
-        <button>Click Me!</button>
+        <button class="set-name">Change Name</button>
         <button class="set-age">Set Random Age</button>
       </div>
     `;
