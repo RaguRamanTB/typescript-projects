b11a08f38498c7fbed9d9f7652d7c07bfdd332bb
RaguRamanTB <aakash1956ragu@gmail.com>
2022-09-30 13:31:00 +0530

    Adding Model Properties

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -1,5 +1,7 @@
+import { User } from '../models/User';
+
 export class UserForm {
-  constructor(public parent: Element | null) {}
+  constructor(public parent: Element | null, public model: User) {}
 
   eventsMap(): { [key: string]: () => void } {
     return {
@@ -20,6 +22,8 @@ export class UserForm {
     return `
       <div>
         <h1>User Form</h1>
+        <div>User Name: ${this.model.get('name')}</div>
+        <div>User Age: ${this.model.get('age')}</div>
         <input />
         <button>Click Me!</button>
       </div>
